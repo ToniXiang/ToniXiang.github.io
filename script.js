e5a314d42cf1670a31fba29e22dd75729b346620
陳國翔 <chen199940@gmail.com>
2023-12-11 16:22:22 +0800

    修正進度條延遲，新增雪花

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-//超連結URL
+function createSnowflake() {
+  const snowflake = document.createElement('div');
+  snowflake.classList.add('snowflake');
+  snowflake.style.left = window.innerWidth/100*getRandomNumber(100) + 'px'; // 雪花始終在視窗的中央生成
+  snowflake.style.animationDuration = getRandomNumber(3) + 2 + 's';
+  snowflake.style.width = getRandomNumber(10) + 10 + 'px';
+  snowflake.style.height = snowflake.style.width;
+
+  document.getElementById('snowflakes').appendChild(snowflake);
+
+  setTimeout(() => {
+    snowflake.remove();
+  }, 5000);
+}
+setInterval(createSnowflake, 100);
 function redirectToPage(url) {
   window.location.href = url;
+}
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * n) + 1;
 }
 //展開/摺疊效果
 document.addEventListener("DOMContentLoaded", function() {
