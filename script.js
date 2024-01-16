88d4a9b271a5075a5daf8616ba18c51996f61081
陳國翔 <chen199940@gmail.com>
2024-01-16 20:57:19 +0800

    重寫自我介紹

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function createSnowflake() {
-  const snowflake = document.createElement('div');
-  snowflake.classList.add('snowflake');
-  snowflake.style.left = window.innerWidth/100*getRandomNumber(100) + 'px'; // 雪花始終在視窗的中央生成
-  snowflake.style.animationDuration = getRandomNumber(3) + 2 + 's';
-  snowflake.style.width = getRandomNumber(10) + 10 + 'px';
-  snowflake.style.height = snowflake.style.width;
-
-  document.getElementById('snowflakes').appendChild(snowflake);
-
-  setTimeout(() => {
-    snowflake.remove();
-  }, 5000);
-}
-setInterval(createSnowflake, 100);
-function redirectToPage(url) {
-  window.location.href = url;
-}
-function getRandomNumber(n) {
-  return Math.floor(Math.random() * n) + 1;
-}
 //展開/摺疊效果
 document.addEventListener("DOMContentLoaded", function() {
   const articles = document.querySelectorAll("#news .news-article");
@@ -28,3 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 });
+//點擊後跳轉頁面
+function redirectToPage(url) {
+  window.location.href = url;
+}
