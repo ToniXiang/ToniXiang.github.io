0569ce6b4d76124a711a483062be534b60030eec
陳國翔 <chen199940@gmail.com>
2024-01-18 23:20:00 +0800

    button修改

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function redirectToPage(url) {
 
 document.addEventListener('DOMContentLoaded', function() {
   //如果頁面在最上方，你可以更改h1元素的透明度
+  var h1 = document.getElementById('main-page');
+  var bn1=document.querySelectorAll('.turnpage-item');
   window.addEventListener('scroll', function() {
-    var h1 = document.getElementById('main-page');
     if (window.scrollY === 0) {
         h1.style.opacity = 1;
+        bn1.forEach(bn=>bn.hidden=false);
     } else {
         h1.style.opacity = 0.2;
+        bn1.forEach(bn=>bn.hidden=true);
     }
   });
 });
