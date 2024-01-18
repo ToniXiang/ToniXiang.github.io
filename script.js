8ea3f73aeec0dfe561aa7d1c496fb0526583d8ab
陳國翔 <chen199940@gmail.com>
2024-01-18 12:48:44 +0800

    優化主頁

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//展開/摺疊效果
-document.addEventListener("DOMContentLoaded", function() {
-  const articles = document.querySelectorAll("#news .news-article");
-  articles.forEach(article => {
-      article.addEventListener("click", function() {
-          this.classList.toggle("expanded");
-      });
-  });
-});
 //點擊後跳轉頁面
 function redirectToPage(url) {
   window.location.href = url;
