bdeddc37d53654db6375fd0e8449a999668a459e
陳國翔 <chen199940@gmail.com>
2024-01-18 18:09:18 +0800

    更改h1元素

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function redirectToPage(url) {
   window.location.href = url;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  //如果頁面在最上方，你可以更改h1元素的透明度
+  window.addEventListener('scroll', function() {
+    var h1 = document.getElementById('main-page');
+    if (window.scrollY === 0) {
+        h1.style.opacity = 1;
+    } else {
+        h1.style.opacity = 0.2;
+    }
+  });
+});
