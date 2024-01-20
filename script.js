77f2c877ef63cc5a4ba163c470062a87b459691b
陳國翔 <chen199940@gmail.com>
2024-01-20 12:23:57 +0800

    作品展示程式碼高亮

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function redirectToPage(url) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  //如果頁面在最上方，你可以更改h1元素的透明度
+  //如果頁面在最上方，你可以更改h1元素的透明度、顯示按鈕
   var h1 = document.getElementById('main-page');
   var bn1=document.querySelectorAll('.turnpage-item');
   window.addEventListener('scroll', function() {
